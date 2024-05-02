598ab55611825a54f91b10156ba873475e1a5641
carlosaguilar96 <102567338+carlosaguilar96@users.noreply.github.com>
2024-05-02 17:50:26 -0600

    Actualización rutas de logos en footer

--- sistemaNotasCS/resources/views/sitioProfesor/footer.blade.php
+++ sistemaNotasCS/resources/views/sitioProfesor/footer.blade.php
@@ -3,7 +3,7 @@
             <nav id="navFooter" class="navbar navbar-expand-lg border-bottom border-body pt-4" >         
                 <div class="container-fluid mx-auto d-block">
                     <div class="row align-items-center justify-content-center">
-                        <div class="col-xl-2 mt-4"><img class="img mx-auto d-block" width="175" src="http://127.0.0.1:8000/img/logo.png" alt=""></div>
+                        <div class="col-xl-2 mt-4"><img class="img mx-auto d-block" width="175" src="{{asset('img/logo.png')}}" alt=""></div>
                         <div class="col-xl-2 col-sm-6 mt-4"><p><span id="titulo">Redes sociales:</span><br><br><a class="link-body-emphasis link-underline link-underline-opacity-0" href="#">Facebook</a><br><br><a class="link-body-emphasis link-underline link-underline-opacity-0" href="#">Instagram</a><br><br><a class="link-body-emphasis link-underline link-underline-opacity-0" href="#">Twitter</a></p></div>
                         <div class="col-xl-5 col-sm-6 mt-4"><p><span id="titulo">Horarios de atención:</span><br><br>Lunes a viernes: 7:00-11:50 - 13:15-17:50<br><br>Dirección: Barrio Mejicano, avenida Morazán, #7-17, Sonsonate, El Salvador<br><br>Teléfonos: 2429-7500, 2429-7502</p></div>
                         <div class="col-12 text-center" style="font-style: italic"><p><br>Copyright ©2024 Colegio Salarrué, All Rights Reserved.</p></div>
